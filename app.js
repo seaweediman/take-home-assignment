22e0e5f3688b872ce71cc72d66d0d9ee3fe43de0
Mohamed-Noriman <seaweed.iman@gmail.com>
2024-05-02 16:11:28 +0800

    Add single object retrieval endpoint

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const PORT = 3001;
+
+// Read sample data from JSON file
+const data = JSON.parse(fs.readFileSync('sample.json'));
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/contact/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const object = data.find(obj => obj.id == id);
+    if (object) {
+        res.json(object);
+    } else {
+        res.status(404).json({ error: 'Object not found' });
+    }
+});
