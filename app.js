bed5cdcaccc2c0b41eff7ce4d5b88f2109d5e969
Mohamed-Noriman <seaweed.iman@gmail.com>
2024-05-02 17:16:19 +0800

    Add logger

--- app.js
+++ app.js
@@ -1,71 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const logger = require('./logger');
 
 const app = express();
 const PORT = 3001;
 
 // Middleware
+// For parsing request bodies
 app.use(bodyParser.json());
+
+// For logging requests
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
 
 // Read sample data from JSON file
 const data = JSON.parse(fs.readFileSync('sample.json'));
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Get object
 app.get('/contact/:id', (req, res) => {
-    // Parse id
-    const id = parseInt(req.params.id);
+  // Parse id
+  const id = parseInt(req.params.id);
 
-    // Search and return object. Else, return error 404
-    const object = data.find(obj => obj.id == id);
-    if (object) {
-        res.json(object);
-    } else {
-        res.status(404).json({ error: 'Object not found' });
-    }
+  // Search and return object. Else, return error 404
+  const object = data.find((obj) => obj.id == id);
+  if (object) {
+    res.json(object);
+  } else {
+    res.status(404).json({ error: 'Object not found' });
+  }
 });
 
 // Add new object
 app.post('/contact', (req, res) => {
-    const newContact = req.body;
+  const newContact = req.body;
 
-    console.log(req.body);
-    // Generate id for new object to be +1 of length, to maintain consistency
-    newContact.id = data.length + 1;
+  console.log(req.body);
+  // Generate id for new object to be +1 of length, to maintain consistency
+  newContact.id = data.length + 1;
 
-    // Add new contact to the parsed array
-    data.push(newContact);
+  // Add new contact to the parsed array
+  data.push(newContact);
 
-    // Write updated data back to the JSON file
-    fs.writeFileSync('sample.json', JSON.stringify(data, null, 2));
+  // Write updated data back to the JSON file
+  fs.writeFileSync('sample.json', JSON.stringify(data, null, 2));
 
-    // Send success response with new contact
-    res.status(200).json(newContact);
+  // Send success response with new contact
+  res.status(200).json(newContact);
 });
 
 app.delete('/contact/:id', (req, res) => {
-    // Parse id
-    const id = parseInt(req.params.id);
+  // Parse id
+  const id = parseInt(req.params.id);
 
-    // Find object
-    const index = data.findIndex(obj => obj.id == id);
+  // Find object
+  const index = data.findIndex((obj) => obj.id == id);
 
-    // If object not found, just return
-    if (index === -1) {
-        return res.status(404).json({ error: 'Object not found' });
-    }
+  // If object not found, just return
+  if (index === -1) {
+    return res.status(404).json({ error: 'Object not found' });
+  }
 
-    // Delete from array
-    data.splice(index, 1);
-    
-    // Write updated data back to the JSON file
-    fs.writeFileSync('sample.json', JSON.stringify(data, null, 2));
+  // Delete from array
+  data.splice(index, 1);
 
-    // Send success response with success message
-    res.status(200).json({ message: `Object with id ${id} has been deleted`});
+  // Write updated data back to the JSON file
+  fs.writeFileSync('sample.json', JSON.stringify(data, null, 2));
+
+  // Send success response with success message
+  res.status(200).json({ message: `Object with id ${id} has been deleted` });
 });
