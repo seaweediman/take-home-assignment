72665c0be64279a50e055d341ea91454e311ab63
Mohamed-Noriman <seaweed.iman@gmail.com>
2024-05-02 16:37:57 +0800

    Add add object API endpoint

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
+
 const app = express();
 const PORT = 3001;
+
+// Middleware
+app.use(bodyParser.json());
 
 // Read sample data from JSON file
 const data = JSON.parse(fs.readFileSync('sample.json'));
@@ -11,12 +16,34 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Get object
 app.get('/contact/:id', (req, res) => {
+    // Parse id
     const id = parseInt(req.params.id);
+
+    // Search and return object. Else, return error 404
     const object = data.find(obj => obj.id == id);
     if (object) {
         res.json(object);
     } else {
         res.status(404).json({ error: 'Object not found' });
     }
+});
+
+// Add new object
+app.post('/contact/:id', (req, res) => {
+    const newContact = req.body;
+
+    console.log(req.body);
+    // Generate id for new object to be +1 of length, to maintain consistency
+    newContact.id = data.length + 1;
+
+    // Add new contact to the parsed array
+    data.push(newContact);
+
+    // Write updated data back to the JSON file
+    fs.writeFileSync('sample.json', JSON.stringify(data, null, 2));
+
+    // Send success response with new contact
+    res.status(200).json(newContact);
 });
